04f29ba27494ea3d9b705253c111464aa8aa2855
Oliver Eglseder <oliver.eglseder@in2code.de>
2017-01-11 09:22:47 +0100

    [TASK] Raise TYPO3 compatibility to 8.5.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,8 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.0.2',
     'constraints' => array(
         'depends' => array(
-            'extbase' => '6.2.0-7.99.99',
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.5.1',
         ),
         'conflicts' => array(),
         'suggests' => array(),
