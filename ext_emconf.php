dc79474dde92318373ab41d8f65f99207e19bfff
Oliver Eglseder <php@vxvr.de>
2019-02-13 12:45:23 +0100

    [META] Add author company and update descriptions

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,11 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'FAL Gallery',
-    'description' => 'Easy to use gallery with separate single-, category- and list view.',
+    'description' => 'Easy to use folder based gallery with separate single-, category- and list view that supports any TYPO3 FAL driver. Official successor of wt_gallery',
     'category' => 'plugin',
     'author' => 'Oliver Eglseder',
     'author_email' => 'php@vxvr.de',
+    'author_company' => 'co-stack.com',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
