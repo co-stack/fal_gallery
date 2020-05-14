4f18423ebd1895ac1619928d6c1f6cea8367a076
Oliver Eglseder <oliver.eglseder@co-stack.com>
2020-05-14 19:13:24 +0200

    [DOCS] Tell everybody that this repo was moved

--- Configuration/TCA/sys_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'fal_gallery',
-    'Configuration/TypoScript',
-    'FAL Gallery'
-);
---
+++ README.md
@@ -0,0 +1,3 @@
+# REPOSITORY MOVED
+
+Please visit https://gitlab.com/co-stack.com/co-stack.com/typo3-extensions/fal_gallery
